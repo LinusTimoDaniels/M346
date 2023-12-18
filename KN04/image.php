665a8fa391a8d54859566488df4fb3f48ec76c06
LinusTimoDaniels <linustimo.daniels@gmail.com>
2023-12-18 08:38:06 +0100

    KN04

---
+++ KN04/image.php
@@ -0,0 +1,14 @@
+	<?php $ihrname = "Linus Daniels"; ?>
+<html>
+	<head>
+	<title><?php echo($ihrname) ?></title>
+	</head>
+	<body>
+		Das ist eine Katze
+	<br />
+	<img src="https://kn04-bucket-linus-daniels.s3.amazonaws.com/eduardo-gorghetto-5auIBbcoRNw-unsplash.jpg" />
+	Platzieren Sie hier notwendige Quellenangaben, z.B. wenn der Inhalt von unsplash kommt.
+	Quellenangaben:
+	    - unsplash.com
+	</body>
+</html>
